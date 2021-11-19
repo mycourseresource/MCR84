541cccd706d5eba1e42b3481c18d637a8070b20f
Stefan <mycourseresource@users.noreply.github.com>
2021-11-19 16:05:13 +0000

    Update serial.sh

--- serial.sh
+++ serial.sh
@@ -5,5 +5,6 @@ read USER_NAME
 FILE_NAME="Serial_${USER_NAME// /_}_$(date +%Y%m%d%H%M%S).txt"
 #FILE_NAME="Stefan_$USER_NAME"
 touch "/home/pi/${FILE_NAME}"
-cat /sys/class/net/wlan0/address >> $FILE_NAME
-cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2 >> $FILE_NAME
+printf "Ethernet MAC:  $(cat /sys/class/net/eth0/address)\n" >> $FILE_NAME
+printf "WiFi MAC:      $(cat /sys/class/net/wlan0/address)\n" >> $FILE_NAME
+printf "CPU Serial No: $(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2)" >> $FILE_NAME
