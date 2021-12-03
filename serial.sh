9d8efd74af45330ef7d31c5abc6f67b3c1c2f88b
Stefan <mycourseresource@users.noreply.github.com>
2021-12-03 09:27:18 +0000

    Update serial.sh

--- serial.sh
+++ serial.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "Serial Number Script"
+echo "Device Serial Number Script"
 echo "Enter your forename followed by the first letter of your surname:"
 read USER_NAME
-FILE_NAME="Serial_${USER_NAME// /_}_$(date +%Y%m%d%H%M%S).txt"
+FILE_NAME="Device_Serial_${USER_NAME// /_}_$(date +%Y%m%d%H%M%S).txt"
 MODEL_NAME=$(tr -d '\0' < /proc/device-tree/model)
 printf "User:           $USER_NAME\n" >> $FILE_NAME
+printf "Class:          CN2\n" >> $FILE_NAME
 printf "Device Model:   $MODEL_NAME\n" >> $FILE_NAME
 printf "$(lsb_release -d)\n" >> $FILE_NAME
 printf "Ethernet MAC:   $(cat /sys/class/net/eth0/address)\n" >> $FILE_NAME
