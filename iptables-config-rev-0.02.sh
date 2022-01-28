45de61f81842103b321d3c6896f2c1466edd2468
Stefan <mycourseresource@users.noreply.github.com>
2022-01-28 12:23:24 +0000

    Create iptables-config-rev-0.02.sh
    This script manages SSH access.
    Only 10.10.10.166 is allowed to start a SSH connection.

---
+++ iptables-config-rev-0.02.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "This is my iptables configuration script."
+echo "Current iptables chains and rules:"
+iptables -S
+read -p  "Replace all chains & rules [yes/n]? " user_input
+if [[ "$user_input" == "yes" ]]; then
+echo "Will start flushing ..."
+iptables -F
+iptables -P INPUT ACCEPT
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+echo "... flushing completed."
+echo "Will start setting up custom chains & rules ..."
+sudo iptables -A INPUT -i lo -j ACCEPT 
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate NEW -p udp --dport 53 -j ACCEPT
+sudo iptables -A INPUT -p tcp -s 10.10.10.166 --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j REJECT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
+echo "... setup completed."
+echo
+echo "New iptables chains and rules:"
+iptables -S
+fi
